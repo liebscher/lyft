35c3d78e3e6278a43ca27de356fe2bb1f02069dc
Alex Liebscher <alexliebscher0@gmail.com>
2019-06-25 12:11:51 -0700

    Updated how driving data are represented

--- CleanData.R
+++ CleanData.R
@@ -8,8 +8,8 @@ data$Date = strptime(data$Date, "%m/%d/%Y")
 data$DOW = factor(format(data$Date, "%u"))
 data$Month = factor(as.numeric(format(data$Date, "%m")))
 
-data$RequestTime = strptime(data$`Request Time`, "%H:%M")
-data$RequestTime = as.numeric(format(data$RequestTime, "%H")) + as.numeric(format(data$RequestTime, "%M")) / 60
+data$StartTime = strptime(data$`Start Time`, "%H:%M")
+data$StartTime = as.numeric(format(data$StartTime, "%H")) + as.numeric(format(data$StartTime, "%M")) / 60
 
 data$PickupTime = strptime(data$`Pickup Time`, "%H:%M")
 data$PickupTime = as.numeric(format(data$PickupTime, "%H")) + as.numeric(format(data$PickupTime, "%M")) / 60
@@ -21,17 +21,17 @@ data$EndTime = as.numeric(format(data$EndTime, "%H")) + as.numeric(format(data$E
 data$Cancel = as.logical(data$Cancel)
 data$Shared = as.logical(data$Shared)
 data$Conversation = as.logical(data$Conversation)
-data$GapTime = data$`Gap Time`
-data$GapAction = data$`Gap Action`
 
 data$StartLocation = data$`Start Location`
 
-data$GasUsage = data$`End Gas` - data$`Start Gas`
+data$StartGas = data$`Start Gas`
+data$EndGas = data$`End Gas`
+data$GasUsage = data$EndGas - data$StartGas
 
-data$Amount = as.numeric(gsub("[$]", "", data$Amount))
-data$Tip = as.numeric(gsub("[$]", "", data$Tip))
-data$Wage = (data$Amount + data$Tip) * (60 / data$Duration)
+data$Earnings = as.numeric(gsub("[$]", "", data$Earnings))
+data$Tips = as.numeric(gsub("[$]", "", data$Tips))
+data$Wage = (data$Earnings + data$Tips) * (60 / data$Duration)
 
-data = data %>% select(-one_of(c("Start Gas", "End Gas", "Gap Time", "Request Time", "Pickup Time", "End Time", "Gap Action", "Start Location", "X3", "X8", "X14")))
+data = data %>% select(-one_of(c("Start Gas", "End Gas", "Start Time", "Pickup Time", "End Time", "Start Location", "X3")))
 
 write_csv(data, "CleanLyftData.csv")
